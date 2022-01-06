1e7c4b071a5d483a2dba5ed63fccc1dc41bb3e2a
asdfkco <rlacksdhr0423@naver.com>
2022-01-07 01:50:22 +0900

    a

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 var beforePosition = document.documentElement.scrollTop
 var scroll_top
-var search_ = document.getElementById('search_head')
 document.addEventListener('scroll', function () {
     var scrolltop = document.documentElement.scrollTop
     var afterPosition = document.documentElement.scrollTop;
@@ -22,31 +21,10 @@ document.addEventListener('scroll', function () {
         scroll_top = false
     }
     if(scroll_top==true){
-        document.getElementById("header_").style.position='relative'
+        document.getElementById("header_").style.top= '36px'
+        // document.getElementById("header_").style.position='static'
     }else{
-        document.getElementById("header_").style.position='fixed'
+        document.getElementById("header_").style.top= '0'
+        // document.getElementById("header_").style.position='fixed'
     }
 });
-window.onload = function () {
-    var header_ = document.getElementById('header_')
-    var search_ = document.getElementById('search_head')
-    var search_input = document.getElementById('search_input')
-    var search_img = document.getElementById('search_png')
-    var close_btu = document.getElementById('close_btu')
-    search_input.addEventListener('click', function () {
-        search_.style.transform='scaleY(1)'
-        header_.style.zIndex='100'
-    });
-    search_img.addEventListener('click', function () {
-        search_.style.transform='scaleY(1)'
-        header_.style.zIndex='100'
-    });
-    close_btu.addEventListener('click', function () {
-        search_.style.transform='scaleY(0)'
-        header_.style.zIndex='999'
-    });
-}
-function banner_close(){
-    var banner = document.getElementById('banner')
-    banner.style.display='none'
-}
